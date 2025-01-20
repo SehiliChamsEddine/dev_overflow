0524a85e7d4d8e9ed07a8c6e795b43ef266eeb40
Sehili Chams Eddine <echams165@gmail.com>
2025-01-20 19:32:30 +0100

    add tailwindcss setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <h1 className=" text-3xl font-black text-violet-900">
-      Welcome To DevOverflow
-    </h1>
-  );
+  return <h1 className=" text-3xl  font-black ">Welcome To DevOverflow</h1>;
 }
