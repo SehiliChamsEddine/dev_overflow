ac95f5b74c02e1a390de7028d3fa6f0c17b138ee
Sehili Chams Eddine <echams165@gmail.com>
2025-01-21 00:37:02 +0100

    add fonts ,assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1 className=" text-3xl  font-black ">Welcome To DevOverflow</h1>;
+  return (
+    <>
+      <h1 className=" h1-bold">Welcome To DevOverflow</h1>
+      <h1 className=" h1-bold  font-space-grotesk  ">Welcome To DevOverflow</h1>
+    </>
+  );
 }
